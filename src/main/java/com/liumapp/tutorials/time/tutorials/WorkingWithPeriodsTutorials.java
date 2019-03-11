21fe3bdcf3259c8df776b2e38f118a89b9c2fea7
liumapp <liumapp.com@gmail.com>
2019-03-11 11:44:55 +0800

    add class WorkingWithPeriodsTutorials

---
+++ src/main/java/com/liumapp/tutorials/time/tutorials/WorkingWithPeriodsTutorials.java
@@ -0,0 +1,23 @@
+package com.liumapp.tutorials.time.tutorials;
+
+import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
+
+/**
+ * file WorkingWithPeriodsTutorials.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/3/11
+ */
+public class WorkingWithPeriodsTutorials implements SimpleTutorials {
+    @Override
+    public String showHelpInfo() {
+        return null;
+    }
+
+    @Override
+    public String run() {
+        return null;
+    }
+}
