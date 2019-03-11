59e0307026cb0c7b77d6fae0bf950ce91c603eba
liumapp <liumapp.com@gmail.com>
2019-03-11 11:46:35 +0800

    add class

--- src/main/java/com/liumapp/tutorials/time/Console.java
+++ src/main/java/com/liumapp/tutorials/time/Console.java
@@ -2,6 +2,10 @@ package com.liumapp.tutorials.time;
 
 import com.liumapp.tutorials.time.enums.Tutorials;
 import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
+import com.liumapp.tutorials.time.tutorials.CreatingDatesAndTimesTutorials;
+import com.liumapp.tutorials.time.tutorials.ManipulatingDatesAndTimesTutorials;
+import com.liumapp.tutorials.time.tutorials.ParsingDatesAndTimesTutorials;
+import com.liumapp.tutorials.time.tutorials.WorkingWithPeriodsTutorials;
 import org.beryx.textio.TextIO;
 import org.beryx.textio.TextIoFactory;
 
@@ -30,6 +34,18 @@ public class Console {
 
     private static void handleTutorial (Tutorials tutorials) {
         switch (tutorials) {
+            case CreatingDatesAndTimesTutorials:
+                running(new CreatingDatesAndTimesTutorials());
+                break;
+            case ManipulatingDatesAndTimesTutorials:
+                running(new ManipulatingDatesAndTimesTutorials());
+                break;
+            case WorkingWithPeriodsTutorials:
+                running(new WorkingWithPeriodsTutorials());
+                break;
+            case ParsingDatesAndTimesTutorials:
+                running(new ParsingDatesAndTimesTutorials());
+                break;
             default:
         }
         showPrimaryMenu();
---
+++ src/main/java/com/liumapp/tutorials/time/tutorials/ParsingDatesAndTimesTutorials.java
@@ -0,0 +1,24 @@
+package com.liumapp.tutorials.time.tutorials;
+
+import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
+
+/**
+ * file ParsingDatesAndTimesTutorials.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/3/11
+ */
+public class ParsingDatesAndTimesTutorials implements SimpleTutorials {
+
+    @Override
+    public String showHelpInfo() {
+        return null;
+    }
+
+    @Override
+    public String run() {
+        return null;
+    }
+}
