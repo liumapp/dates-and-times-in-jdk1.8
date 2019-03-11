08b624a8fc0b88b7de777fe5bcf20fe37475f5a0
liumapp <liumapp.com@gmail.com>
2019-03-11 11:30:34 +0800

    add basic enums , interfaces and tutorials

---
+++ src/main/java/com/liumapp/tutorials/time/enums/Tutorials.java
@@ -0,0 +1,15 @@
+package com.liumapp.tutorials.time.enums;
+
+/**
+ * file Tutorials.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/1/25
+ */
+public enum Tutorials {
+
+
+
+}
---
+++ src/main/java/com/liumapp/tutorials/time/interfaces/SimpleTutorials.java
@@ -0,0 +1,26 @@
+package com.liumapp.tutorials.time.interfaces;
+
+/**
+ * file SimpleTutorials.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/1/25
+ */
+public interface SimpleTutorials {
+
+    /**
+     * show tutorials detail info
+     * @return string help info
+     */
+    public String showHelpInfo();
+
+    /**
+     * running the tutorials
+     * @return running results
+     */
+    public String run();
+
+
+}
