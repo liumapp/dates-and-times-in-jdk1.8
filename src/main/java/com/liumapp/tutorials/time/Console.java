83754d3cd404dc3f74277166d0f9b67dbc0f72ae
liumapp <liumapp.com@gmail.com>
2019-03-11 11:29:10 +0800

    init src folder

---
+++ src/main/java/com/liumapp/tutorials/time/Console.java
@@ -0,0 +1,15 @@
+package com.liumapp.tutorials.time;
+
+/**
+ * file Console.java
+ * author liumapp
+ * github https://github.com/liumapp
+ * email liumapp.com@gmail.com
+ * homepage http://www.liumapp.com
+ * date 2019/3/11
+ */
+public class Console {
+
+
+
+}
