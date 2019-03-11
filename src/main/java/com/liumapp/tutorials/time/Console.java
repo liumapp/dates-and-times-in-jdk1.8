40e5c84e10d2dd1368fe2ebd78f3287d535bf08f
liumapp <liumapp.com@gmail.com>
2019-03-11 11:34:25 +0800

    init Console class

--- src/main/java/com/liumapp/tutorials/time/Console.java
+++ src/main/java/com/liumapp/tutorials/time/Console.java
@@ -1,5 +1,10 @@
 package com.liumapp.tutorials.time;
 
+import com.liumapp.tutorials.time.enums.Tutorials;
+import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
+import org.beryx.textio.TextIO;
+import org.beryx.textio.TextIoFactory;
+
 /**
  * file Console.java
  * author liumapp
@@ -10,6 +15,30 @@ package com.liumapp.tutorials.time;
  */
 public class Console {
 
+    public static TextIO textIO;
+
+    public static void main (String[] args) {
+        textIO = TextIoFactory.getTextIO();
+        showPrimaryMenu();
+    }
+
+    private static void showPrimaryMenu () {
+        Tutorials tutorials = textIO.newEnumInputReader(Tutorials.class)
+                .read("What kind of tutorials are you interested in ?");
+        handleTutorial(tutorials);
+    }
+
+    private static void handleTutorial (Tutorials tutorials) {
+        switch (tutorials) {
+            default:
+        }
+        showPrimaryMenu();
+    }
+
+    private static void running (SimpleTutorials simpleTutorials) {
+        textIO.getTextTerminal().println(simpleTutorials.showHelpInfo());
+        textIO.getTextTerminal().println(simpleTutorials.run());
+    }
 
 
 }
