10c2f74acfe7b2eda9eeb86ec62e10bc1b9e17c1
liumapp <liumapp.com@gmail.com>
2019-03-11 14:12:09 +0800

    fixed show help info of creatime dates and times

--- src/main/java/com/liumapp/tutorials/time/tutorials/CreatingDatesAndTimesTutorials.java
+++ src/main/java/com/liumapp/tutorials/time/tutorials/CreatingDatesAndTimesTutorials.java
@@ -13,7 +13,11 @@ import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
 public class CreatingDatesAndTimesTutorials implements SimpleTutorials {
     @Override
     public String showHelpInfo() {
-        return null;
+        return "* LocalDate 只有日期，没有时间戳和时区的概念\n" +
+                "* LocalTime 只有时间戳，没有日期和时区的概念\n" +
+                "* LocalDateTime 有日期和时间戳，但是没有时区的概念\n" +
+                "* ZonedDateTime 有日期、时间戳和时区\n" +
+                "Oracle建议我们忽略时区，除非真的必须要用到它。（不过企业应用一般都是要考虑这个玩意儿）";
     }
 
     @Override
