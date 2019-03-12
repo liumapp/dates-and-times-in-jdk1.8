edf55e945ac2b2200c6512e233608bef616b2d24
liumapp <liumapp.com@gmail.com>
2019-03-12 11:40:08 +0800

    manipulating dates tutorials done

--- src/main/java/com/liumapp/tutorials/time/tutorials/ManipulatingDatesAndTimesTutorials.java
+++ src/main/java/com/liumapp/tutorials/time/tutorials/ManipulatingDatesAndTimesTutorials.java
@@ -4,6 +4,8 @@ import com.liumapp.tutorials.time.Console;
 import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
 
 import java.time.ZonedDateTime;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * file ManipulatingDatesAndTimesTutorials.java
@@ -34,7 +36,7 @@ public class ManipulatingDatesAndTimesTutorials implements SimpleTutorials {
      * jdk1.8版本的在已有的日期上添加秒、分、时、日、周、月、年等信息
      */
     private void demo1 () {
-        Console.textIO.getTextTerminal().println("demo1演示在已有的日期上添加日、周、月、年等信息：");
+        Console.textIO.getTextTerminal().println("demo1演示jdk1.8版本的在已有的日期上添加秒、分、时、日、周、月、年等信息：");
         ZonedDateTime date = ZonedDateTime.now();
         Console.textIO.getTextTerminal().println("当前日期为：ZonedDateTime.now() = " + date);
         Console.textIO.getTextTerminal().println("增加20秒的方法：date.plusSeconds(20) = " + date.plusSeconds(20));
@@ -43,14 +45,21 @@ public class ManipulatingDatesAndTimesTutorials implements SimpleTutorials {
         Console.textIO.getTextTerminal().println("增加一天的方法： date.plusDays(1) = " + date.plusDays(1));
         Console.textIO.getTextTerminal().println("增加一周的方法： date.plusWeeks(1) = " + date.plusWeeks(1));
         Console.textIO.getTextTerminal().println("增加一个月的方法：date.plusMonths(1) = " + date.plusMonths(1));
-        Console.textIO.getTextTerminal().println("增加一年的方法：date.plusYears(1) = " + date.plusYears(1));
+        Console.textIO.getTextTerminal().println("增加一年的方法：date.plusYears(1) = s" + date.plusYears(1));
     }
 
     /**
      * jdk1.7及更早版本的在已有的日期上添加秒、分、时、日、周、月、年等信息
      */
     private void demo2 () {
-
+        Console.textIO.getTextTerminal().println("demo2演示jdk1.7及更早版本的在已有的日期上添加秒、分、时、日、周、月、年等信息");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        Console.textIO.getTextTerminal().println("获取到的当前时间为：" + calendar.getTime());
+        calendar.add(Calendar.DATE, 1);
+        Console.textIO.getTextTerminal().println("增加一天的方法为：calendar.add(Calendar.DATE, 1)，执行后的日期为： " + calendar.getTime());
+        calendar.add(Calendar.DATE, -2);
+        Console.textIO.getTextTerminal().println("减少两天的方法为：calendar.add(Calendar.DATE, -2)，执行后的日期为：" + calendar.getTime());
     }
 
 }
