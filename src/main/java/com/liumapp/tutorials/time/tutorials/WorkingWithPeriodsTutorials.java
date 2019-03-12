b6d73d251135b5fb57f5f5e212fe7cce0e111a50
liumapp <liumapp.com@gmail.com>
2019-03-12 14:55:05 +0800

    demo1 done in working periods

--- src/main/java/com/liumapp/tutorials/time/tutorials/WorkingWithPeriodsTutorials.java
+++ src/main/java/com/liumapp/tutorials/time/tutorials/WorkingWithPeriodsTutorials.java
@@ -1,6 +1,12 @@
 package com.liumapp.tutorials.time.tutorials;
 
+import com.liumapp.tutorials.time.Console;
 import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 /**
  * file WorkingWithPeriodsTutorials.java
@@ -26,7 +32,20 @@ public class WorkingWithPeriodsTutorials implements SimpleTutorials {
         return "done...\n";
     }
 
+    /**
+     * 设置两个时间点，时间A与时间B，当时间A小于时间B的时候，执行输出动作，并对A递增一个月，直到A大于B为止
+     */
     private void demo1 () {
-
+        Console.textIO.getTextTerminal().println("设置两个时间点，时间A与时间B，当时间A小于时间B的时候，执行输出动作，并对A递增一个月，直到A大于B为止");
+        LocalDate A = LocalDate.of(2019, Month.MARCH, 1);
+        LocalDate B = LocalDate.of(2019, Month.JULY, 30);
+        Console.textIO.getTextTerminal().println("A is : " + A + " B is : " + B);
+        while (A.isBefore(B)) {
+            Console.textIO.getTextTerminal().println("the system found A is early than B , so print the msg");
+            Console.textIO.getTextTerminal().println("A is : " + A + " B is : " + B);
+            A.plusMonths(1);
+        }
+        Console.textIO.getTextTerminal().println("Now A is late than B ...");
+        Console.textIO.getTextTerminal().println("A is : " + A + " B is : " + B);
     }
 }
