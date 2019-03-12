88a1befcfc17b84d4d7996f56548c5a1e6c93322
liumapp <liumapp.com@gmail.com>
2019-03-12 09:21:40 +0800

    ready to update demo2 of creating dates and times tutorials

--- src/main/java/com/liumapp/tutorials/time/tutorials/CreatingDatesAndTimesTutorials.java
+++ src/main/java/com/liumapp/tutorials/time/tutorials/CreatingDatesAndTimesTutorials.java
@@ -27,12 +27,16 @@ public class CreatingDatesAndTimesTutorials implements SimpleTutorials {
     public String run() {
         try {
             this.demo1();
+            this.demo2();
         } catch (Exception e) {
             e.printStackTrace();
         }
         return "done...";
     }
 
+    /**
+     * jdk1.8中使用时间的最简单方式
+     */
     private void demo1 () {
         Console.textIO.getTextTerminal().println("demo1介绍了用最简单的方式，使用jdk1.8所提供的时间方法来获取当前时间：");
         Console.textIO.getTextTerminal().println("a simple demo of LocalDate : " + LocalDate.now());
@@ -42,6 +46,18 @@ public class CreatingDatesAndTimesTutorials implements SimpleTutorials {
         Console.textIO.getTextTerminal().println("a simple demo of ZonedDateTime :" + ZonedDateTime.now());
         Console.textIO.getTextTerminal().println("使用LocalDate获取指定的日期：");
         Console.textIO.getTextTerminal().println("获取2017年1月20日的日期: LocalDate.of(2017, Month.JANUARY, 20) = " + LocalDate.of(2017, Month.JANUARY, 20));
+        Console.textIO.getTextTerminal().println("使用LocalTime获取指定的时间：");
+        Console.textIO.getTextTerminal().println("获取19点35分12秒的时间： LocalTime.of(19, 35, 12) = " + LocalTime.of(19, 35, 12));
+        Console.textIO.getTextTerminal().println("使用LocalDateTime获取指定的日期跟时间：");
+        Console.textIO.getTextTerminal().println("获取2017年1月20日19点35分12秒的时间：LocalDateTime.of(2017, Month.JANUARY, 20, 19, 35, 12) = " + LocalDateTime.of(2017, Month.JANUARY, 20, 19, 35, 12));
+        Console.textIO.getTextTerminal().println("需要注意一点，LocalDate, LocalTime, LocalDateTime的源码中，都是使用的私有构造方法，这意味着任何new LocalDate()等写法都是无法通过编译的，我们只能使用它们三者的静态方法");
+    }
+
+    /**
+     * 在jdk1.7和更早版本的时间用法
+     */
+    private void demo2 () {
+        Console.textIO.getTextTerminal().println("demo2介绍了jdk1.7跟更早版本之前的时间方法：");
         
     }
 }
