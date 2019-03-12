372991e34e456bef21a7f30d363b96b6be06fede
liumapp <liumapp.com@gmail.com>
2019-03-12 10:24:15 +0800

    demo1 for maniputating done

--- src/main/java/com/liumapp/tutorials/time/tutorials/ManipulatingDatesAndTimesTutorials.java
+++ src/main/java/com/liumapp/tutorials/time/tutorials/ManipulatingDatesAndTimesTutorials.java
@@ -3,8 +3,6 @@ package com.liumapp.tutorials.time.tutorials;
 import com.liumapp.tutorials.time.Console;
 import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 /**
@@ -25,6 +23,7 @@ public class ManipulatingDatesAndTimesTutorials implements SimpleTutorials {
     public String run() {
         try {
             this.demo1();
+            this.demo2();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -32,12 +31,26 @@ public class ManipulatingDatesAndTimesTutorials implements SimpleTutorials {
     }
 
     /**
-     * 在已有的日期上添加日、周、月、年等信息
+     * jdk1.8版本的在已有的日期上添加秒、分、时、日、周、月、年等信息
      */
     private void demo1 () {
         Console.textIO.getTextTerminal().println("demo1演示在已有的日期上添加日、周、月、年等信息：");
         ZonedDateTime date = ZonedDateTime.now();
-        Console.textIO.getTextTerminal().println("增加一天的方法： date.plusDays(1) + " + date.plusDays(1));
-        
+        Console.textIO.getTextTerminal().println("当前日期为：ZonedDateTime.now() = " + date);
+        Console.textIO.getTextTerminal().println("增加20秒的方法：date.plusSeconds(20) = " + date.plusSeconds(20));
+        Console.textIO.getTextTerminal().println("增加10分钟的方法：date.plusMinutes(10) = " + date.plusMinutes(10));
+        Console.textIO.getTextTerminal().println("增加3小时的方法：date.plusHours(3) = " + date.plusHours(3));
+        Console.textIO.getTextTerminal().println("增加一天的方法： date.plusDays(1) = " + date.plusDays(1));
+        Console.textIO.getTextTerminal().println("增加一周的方法： date.plusWeeks(1) = " + date.plusWeeks(1));
+        Console.textIO.getTextTerminal().println("增加一个月的方法：date.plusMonths(1) = " + date.plusMonths(1));
+        Console.textIO.getTextTerminal().println("增加一年的方法：date.plusYears(1) = " + date.plusYears(1));
     }
+
+    /**
+     * jdk1.7及更早版本的在已有的日期上添加秒、分、时、日、周、月、年等信息
+     */
+    private void demo2 () {
+
+    }
+
 }
