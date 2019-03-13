47c03a6544a5f9837736972e078eb3ea745fecf3
liumapp <liumapp.com@gmail.com>
2019-03-13 09:43:45 +0800

    parsing tutorials done

--- src/main/java/com/liumapp/tutorials/time/tutorials/ParsingDatesAndTimesTutorials.java
+++ src/main/java/com/liumapp/tutorials/time/tutorials/ParsingDatesAndTimesTutorials.java
@@ -1,6 +1,11 @@
 package com.liumapp.tutorials.time.tutorials;
 
+import com.liumapp.tutorials.time.Console;
 import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * file ParsingDatesAndTimesTutorials.java
@@ -14,7 +19,7 @@ public class ParsingDatesAndTimesTutorials implements SimpleTutorials {
 
     @Override
     public String showHelpInfo() {
-        return null;
+        return "ParsingDatesAndTimesTutorials演示如何将一个时间字符串转换成具体的日期对象：LocalDate, LocalDateTime, LocalTime等等";
     }
 
     @Override
@@ -27,7 +32,15 @@ public class ParsingDatesAndTimesTutorials implements SimpleTutorials {
         return "done...\n";
     }
 
+    /**
+     * 时间字符串转换为LocalDate与LocalTime对象
+     */
     private void demo1 () {
-
+        Console.textIO.getTextTerminal().println("demo1演示了如何把时间字符串转换为LocalDate与LocalTime对象");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDate date = LocalDate.parse("2019-03-13", dateTimeFormatter);
+        LocalTime time = LocalTime.parse("19:23");
+        Console.textIO.getTextTerminal().println("获取到的date对象为：" + date + "\n" +
+                "获取到的time对象为：" + time);
     }
 }
