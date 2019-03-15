9f0a648d2a95296e8a29d9b11e0dd9779220a7e2
liumapp <liumapp.com@gmail.com>
2019-03-15 20:07:33 +0800

    demo1 for chronoUnits done

--- src/main/java/com/liumapp/tutorials/time/tutorials/UsingChronoUnitTutorials.java
+++ src/main/java/com/liumapp/tutorials/time/tutorials/UsingChronoUnitTutorials.java
@@ -1,7 +1,13 @@
 package com.liumapp.tutorials.time.tutorials;
 
+import com.liumapp.tutorials.time.Console;
 import com.liumapp.tutorials.time.helper.ConsoleHelper;
 import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 
 /**
  * file UsingChronoUnitTutorials.java
@@ -14,7 +20,7 @@ import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
 public class UsingChronoUnitTutorials implements SimpleTutorials {
     @Override
     public String showHelpInfo() {
-        return null;
+        return "ChronoUnit可以帮助我们用不同的时间单位衡量两个日期之间的间距";
     }
 
     @Override
@@ -28,7 +34,20 @@ public class UsingChronoUnitTutorials implements SimpleTutorials {
         return "done...\n";
     }
 
+    /**
+     * 用不同的时间单位衡量两个日期之间的间距
+     */
     private void demo1 () {
-
+        Console.textIO.getTextTerminal().println("demo1演示用不同的时间单位，来衡量1978年3月2号0时0分距离此时此刻的间距");
+        LocalDateTime old = LocalDateTime.of(1978, Month.MARCH, 2, 0, 0);
+        LocalDateTime now = LocalDateTime.now();
+        long years = ChronoUnit.YEARS.between(old, now);
+        long months = ChronoUnit.MONTHS.between(old, now);
+        long days = ChronoUnit.DAYS.between(old, now);
+        long hours = ChronoUnit.HOURS.between(old, now);
+        long mins = ChronoUnit.MINUTES.between(old, now);
+        //你也可以用秒、微妙、纳秒这些单位
+        Console.textIO.getTextTerminal().println("1978年3月2号0时0分距离现在" + now + "\n" +
+                "隔了:" + years + "年\n或者" + months + "月\n或者" + days + "天\n或者" + hours + "小时\n或者" + mins + "分钟\n");
     }
 }
