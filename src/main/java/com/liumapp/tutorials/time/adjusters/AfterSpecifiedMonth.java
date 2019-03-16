dccd6af8b618092778b0a645ef279ae4c54beaa2
liumapp <liumapp.com@gmail.com>
2019-03-16 11:36:19 +0800

    added specified month temporal

--- src/main/java/com/liumapp/tutorials/time/adjusters/AfterSpecifiedMonth.java
+++ src/main/java/com/liumapp/tutorials/time/adjusters/AfterSpecifiedMonth.java
@@ -1,21 +1,28 @@
 package com.liumapp.tutorials.time.adjusters;
 
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
 import java.time.temporal.TemporalAdjuster;
 
 /**
- * file NextChristmas.java
+ * file AfterSpecifiedMonth.java
  * author liumapp
  * github https://github.com/liumapp
  * email liumapp.com@gmail.com
  * homepage http://www.liumapp.com
  * date 2019/3/16
  */
-public class NextChristmas implements TemporalAdjuster {
+public class AfterSpecifiedMonth implements TemporalAdjuster {
+
+    private int monthNumber;
+
+    public AfterSpecifiedMonth(int monthNumber) {
+        this.monthNumber = monthNumber;
+    }
 
     @Override
     public Temporal adjustInto(Temporal temporal) {
-        return temporal.with(ChronoField.MONTH_OF_YEAR, 12).with(ChronoField.DAY_OF_MONTH, 25);
+        return temporal.with(ChronoField.PROLEPTIC_MONTH, monthNumber);
     }
 }
--- src/main/java/com/liumapp/tutorials/time/tutorials/UsingTemporalAdjustersTutorials.java
+++ src/main/java/com/liumapp/tutorials/time/tutorials/UsingTemporalAdjustersTutorials.java
@@ -1,8 +1,11 @@
 package com.liumapp.tutorials.time.tutorials;
 
 import com.liumapp.tutorials.time.Console;
+import com.liumapp.tutorials.time.adjusters.AfterSpecifiedMonth;
+import com.liumapp.tutorials.time.adjusters.NextChristmas;
 import com.liumapp.tutorials.time.helper.ConsoleHelper;
 import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
+import net.bytebuddy.asm.Advice;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
@@ -56,6 +59,11 @@ public class UsingTemporalAdjustersTutorials implements SimpleTutorials {
      */
     private void demo2 () {
         Console.textIO.getTextTerminal().println("demo2演示了如何创建自定义的TemporalAdjusters");
-        
+        LocalDate localDate = LocalDate.now();
+        LocalDate nextChristmas = localDate.with(new NextChristmas());
+        LocalDate after13Month = localDate.with(new AfterSpecifiedMonth(13));
+        Console.textIO.getTextTerminal().println("借助Custom TemporalAdjusters获取到的下一个圣诞节为：" + nextChristmas + "\n" +
+                "13个月后的日期为：" + after13Month);
+
     }
 }
