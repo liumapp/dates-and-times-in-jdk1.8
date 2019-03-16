2cdb5c772fdba14434dff1aa09f014aa679e1e2d
liumapp <liumapp.com@gmail.com>
2019-03-16 18:53:39 +0800

    demo1 for clock doen

--- src/main/java/com/liumapp/tutorials/time/tutorials/UsingClockTutorials.java
+++ src/main/java/com/liumapp/tutorials/time/tutorials/UsingClockTutorials.java
@@ -34,28 +34,24 @@ public class UsingClockTutorials implements SimpleTutorials {
     }
 
     /**
-     * get instant by clock
+     * clock的一些简单方法
      */
     private void demo1 () {
-        Console.textIO.getTextTerminal().println("demo1演示了通过clock获取系统当前时间戳");
+        Console.textIO.getTextTerminal().println("demo1演示了clock的一些简单方法");
         Clock clock = Clock.systemDefaultZone();
         Instant instant = clock.instant();
         Clock clock1 = Clock.systemUTC();
         Clock clock2 = Clock.system(ZoneId.of("GMT+2"));
-
 
         Console.textIO.getTextTerminal().println("通过clock获取系统时间戳： " + instant);
         Console.textIO.getTextTerminal().println("通过clock获取系统时间戳的第二种方式（UTC）：" + clock1.instant());
         Console.textIO.getTextTerminal().println("通过clock获取东二时区的时间：" + clock2.instant());
         Console.textIO.getTextTerminal().println("通过clock获取本地系统的时区：" + clock + "，clock的源码中也是通过ZoneId.systemDefault()来获取本地系统的时区，所以我们直接使用ZoneId.systemDefault()也是可以的");
         Console.textIO.getTextTerminal().println("通过clock获取系统当前时间戳的微妙数，虽然我们平时更喜欢用System.currentTimeInMillis()，但使用clock.millis()更高效，当前时间戳的微秒数为：" + clock.millis());
-
-
-
     }
 
     /**
-     *
+     * 
      */
     private void demo2 () {
 
