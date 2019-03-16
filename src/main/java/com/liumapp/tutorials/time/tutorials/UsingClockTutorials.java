25800821aae626478018c0b46260be28b2f6650e
liumapp <liumapp.com@gmail.com>
2019-03-16 18:45:23 +0800

    update demo1 in clock tutorials

--- src/main/java/com/liumapp/tutorials/time/tutorials/UsingClockTutorials.java
+++ src/main/java/com/liumapp/tutorials/time/tutorials/UsingClockTutorials.java
@@ -1,6 +1,7 @@
 package com.liumapp.tutorials.time.tutorials;
 
 import com.liumapp.tutorials.time.Console;
+import com.liumapp.tutorials.time.helper.ConsoleHelper;
 import com.liumapp.tutorials.time.interfaces.SimpleTutorials;
 
 import java.time.Clock;
@@ -24,6 +25,7 @@ public class UsingClockTutorials implements SimpleTutorials {
     public String run() {
         try {
             this.demo1();
+            ConsoleHelper.showDividingLine();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -34,11 +36,20 @@ public class UsingClockTutorials implements SimpleTutorials {
      * get instant by clock
      */
     private void demo1 () {
+        Console.textIO.getTextTerminal().println("demo1演示了通过clock获取系统当前时间戳");
         Clock clock = Clock.systemDefaultZone();
         Instant instant = clock.instant();
+        Clock clock1 = Clock.systemUTC();
+
         Console.textIO.getTextTerminal().println("通过clock获取系统时间戳： " + instant);
+        Console.textIO.getTextTerminal().println("通过clock获取系统时间戳的第二种方式（UTC）：" + clock1.instant());
+
+
     }
 
+    /**
+     *
+     */
     private void demo2 () {
 
     }
